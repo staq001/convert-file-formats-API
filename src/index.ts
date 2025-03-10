912dd51c61491055f234e7dada9f9a0dd9958fe5
staq001 <dame.hush@gmail.com>
2025-03-10 02:42:36 +0100

    little code formatting.

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,8 @@ import path from "node:path";
 const PORT = process.env.PORT || 3555;
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicDirectoryPath));
+app.use(express.static("./public"));
 
 app.use(express.json());
 app.use(pdfToWordRouter);
